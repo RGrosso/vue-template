a1fece4dc3c7c9bffde7e528c9cd84834e75857e
Ryan Grosso <ryangrosso@outlook.com>
2022-11-08 20:55:58 +0000

    Eslint, Prettier and vue router update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,62 @@
 /* eslint-env node */
+/* eslint-disable @typescript-eslint/naming-convention */
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "@vue/eslint-config-typescript",
     "@vue/eslint-config-prettier",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
   parserOptions: {
     ecmaVersion: "latest",
+  },
+  rules: {
+    "@typescript-eslint/no-inferrable-types": "off",
+    "vue/attribute-hyphenation": "off",
+    "vue/html-self-closing": "off",
+    "vue/max-attributes-per-line": "off",
+    "vue/multi-word-component-names": "off",
+    "vue/singleline-html-element-content-newline": "off",
+    "vue/v-on-event-hyphenation": "off",
+    "id-length": "off",
+    "vue/html-indent": [
+      "warn",
+      2,
+      {
+        attribute: 1,
+        baseIndent: 1,
+        closeBracket: 0,
+        alignAttributesVertically: true,
+        ignores: ["VAttribute"],
+      },
+    ],
+    "quote-props": ["error", "as-needed"],
+    "@typescript-eslint/naming-convention": [
+      "error",
+      { selector: "default", format: ["camelCase"] },
+      { selector: "variable", format: ["camelCase", "UPPER_CASE"] },
+      { selector: "parameter", format: ["camelCase"], leadingUnderscore: "allow" },
+      {
+        selector: "memberLike",
+        modifiers: ["private"],
+        format: ["camelCase"],
+        leadingUnderscore: "require",
+      },
+      { selector: "typeLike", format: ["PascalCase"] },
+      { selector: ["classMethod", "classProperty"], format: ["camelCase"] },
+      { selector: "enumMember", format: ["PascalCase"] },
+      { selector: "objectLiteralProperty", format: ["PascalCase", "camelCase", "UPPER_CASE", "snake_case"] },
+    ],
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
   },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,32 @@
-import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const inProduction = mode === "production";
+
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: "[name].js",
+          chunkFileNames: "[name].js",
+          assetFileNames: "[name].[ext]",
+        },
+      },
+      sourcemap: !inProduction,
+      target: "esnext",
+      minify: inProduction ? "esbuild" : false,
+    },
+    server: {
+      port: 3333,
+    },
+  };
 });
